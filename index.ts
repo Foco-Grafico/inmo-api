2596de7647cbf8f9c3b1c69f0a1e8345cb87521c
KikeDeAlba <likingvirus1@gmail.com>
2024-05-14 14:46:39 -0700

    Remove unused code for database connection and user entity

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { createApp, serveApp } from '@/core/app'
-import 'reflect-metadata'
 
 serveApp(createApp())
   .catch(console.error)
--- src/app/auth/auth.router.ts
+++ src/app/auth/auth.router.ts
@@ -1,7 +1,5 @@
 import { createRouter } from '@/core/routers'
 import multer from 'multer'
-import { createDbConn } from 'app-data-source'
-import { User } from '../../entity/user.entity'
 
 const storage = multer.memoryStorage()
 
@@ -12,12 +10,7 @@ const authRouter = createRouter({
 })
 
 authRouter.module.get('/login', upload.none(), (req, res) => {
-  const db = createDbConn()
-
-  db.getRepository(User).count().then(count => {
-    res.json({ count })
-  })
-    .catch(console.error)
+  res.send('Login')
 })
 
 export default authRouter
